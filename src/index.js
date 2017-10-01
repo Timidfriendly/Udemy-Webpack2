06b5bc8e640a920308cd5b81cc88a8310b4e2bc3
Timidfriendly <timid@point-online.org>
2017-10-01 19:15:39 +0200

    ADD image viewer module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import sum from './sum.js';
+import sum from './sum';
+import './image_viewer' // Because we don't export any code from this file. Just gets run immediately
 
 const total = sum(10,5);
 console.log(total);
