090df7ba0f4393dd5d96d5024d8b47ae4fa0ae5c
Timidfriendly <timid@point-online.org>
2017-10-01 19:20:45 +0200

    REFACTOR js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-    entry: "./src/index.js",
+    entry: "./src/js/index.js",
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "bundle.js"
